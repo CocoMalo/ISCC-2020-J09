ad71da3fa771139e62535f6532ca451d9df32266
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-06 11:59:58 +0200

    Exo 1

---
+++ EX_01/connexion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+
+    <body>
+        <form> <method ="POST" action= "/securite.php">
+        
+        <input type ="text" name= "login" placeholder= "Login">
+        <input type ="text" name= "password" placeholder = "Mot de passe">
+
+    </body>
+</html>
